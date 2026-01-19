9d2e370be4c940db953c903aa55e228ef3368c02
maheshgoyal-sys <maheshgoyal20032004@gmail.com>
2026-01-19 12:37:03 +0530

    add three pages

--- src/main/java/com/example/day2/Home.java
+++ src/main/java/com/example/day2/Home.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+
 @Controller
+
 public class Home {
     @GetMapping("/")
     public String hello(){
-        return "index";
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
     }
 }
